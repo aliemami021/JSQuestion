d021a23508e67e3955786891b1a7e07c57927bf8
ali emami <bmd.aliemami@gmail.com>
2023-08-15 17:51:53 +0330

    fitst commit

---
+++ arrays/app.js
@@ -0,0 +1,17 @@
+// first
+function myFunc(arr, func) {
+  for (let i = 0; i < arr.length; i++) {
+    func(arr[i], i, arr);
+  }
+}
+
+
+
+function func(item, index, array) {
+  
+}
+
+myFunc(test, func);
+
+// secound
+
